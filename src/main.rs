2091b024390ca30bf27e69111423e362053dcae6
c.victor <cesarvictor@hotmail.fr>
2024-05-30 12:04:01 +0200

    Initial commit + Setup Rocket

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/")]
+fn about() -> &'static str {
+    "About me"
+}
+
+#[get("/")]
+fn contact() -> &'static str {
+    "Contact me"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/about", routes![about])
+        .mount("/contact", routes![contact])
+}

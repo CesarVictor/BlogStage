553823a5bef9fbc3cb050e17899f91b273fa046a
c.victor <cesarvictor@hotmail.fr>
2024-05-31 11:29:01 +0200

    Add diesel methods for post +  starting routing rocket

---
+++ src/bin/delete_post.rs
@@ -0,0 +1,17 @@
+use diesel::prelude::*;
+use my_blog::*;
+use std::env::args;
+
+fn main() {
+    use self::schema::posts::dsl::*;
+
+    let target = args().nth(1).expect("delete_post requires a post id");
+    let pattern = format!("%{}%", target);
+
+    let connection = &mut establish_connection();
+    let num_deleted = diesel::delete(posts.filter(title.like(pattern)))
+        .execute(connection)
+        .expect("Error deleting posts");
+    println!("Deleted {} posts", num_deleted);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate rocket;
 
+
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
